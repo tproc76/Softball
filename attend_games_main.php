902c0b1b7c4c0fb420d15e6c6c0164366f4c0f14
tproc76 <proc@comcast.net>
2022-02-02 08:21:34 -0500

    Added Footer

--- attend_games_main.php
+++ attend_games_main.php
@@ -16,6 +16,7 @@
 
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="bootstrap-4.0.0-alpha.6-dist/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="page_format.css">
 
 		<script type="text/javascript" src="dist-js/jquery-3.2.1.min.js"></script>
 		<script type="text/javascript" src="attend_games_manage.js"></script>
@@ -94,6 +95,23 @@ $select = mysqli_query($link, "SELECT * FROM attend_games WHERE team_id='$team_i
 <br>
 <div class="row justify-content-md-center">
 	<input type="button" value="Complete" onclick="move_page(<?php echo $team_id;?>);">
+</div>
+
+<div class="footer">
+	<?php  
+	$fullfilename = $_SERVER['REQUEST_URI']; 
+	$fileparts = explode("?",basename($fullfilename));
+	$filename = $fileparts[0];
+	//echo $filename;
+	//echo filemtime($filename);
+	
+	date_default_timezone_set('US/Eastern');
+	// checking last time the contents of
+	// a file were changed and formatting
+	// the output of the date 
+	//echo "Version:".date("y.md.Hi", filemtime($filename));
+	echo "Updated: ".date("F d Y H:i:s", filemtime($filename));
+	?>
 </div>
 
 </body>
