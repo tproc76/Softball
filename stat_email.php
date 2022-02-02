91206bddc48f0159bf480429584116a4ae954540
tproc76 <proc@comcast.net>
2022-02-02 09:02:32 -0500

    Added the ability to send individual emails when mass email fails

--- stat_email.php
+++ stat_email.php
@@ -1,6 +1,6 @@
 <?php
 include 'db_setup.php';
-
+echo '<link rel="stylesheet" type="text/css" href="page_format.css">';
 $sendEmail = true;
 
 $returnResults = array();
@@ -26,66 +26,76 @@ if ($result = mysqli_query($link, $findTeamInfo))
 		{
 		$theStatTeam = mysqli_fetch_array($result, MYSQLI_ASSOC);
 		
-		$statteamid = $theStatTeam['team_id'];
-		
-		$findStatInfo = "SELECT p.email AS email FROM attend_players p JOIN attend_members m ON p.player_id=m.player_id WHERE m.team_id='$statteamid'";
-		
-		if ($result = mysqli_query($link, $findStatInfo))
-			{
-			$maillist = "";
+		if ($theStatTeam != null)
+			{		
+			$statteamid = $theStatTeam['team_id'];
 			
-			while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) 
+			$findStatInfo = "SELECT p.email AS email FROM attend_players p JOIN attend_members m ON p.player_id=m.player_id WHERE m.team_id='$statteamid'";
+			
+			if ($result = mysqli_query($link, $findStatInfo))
 				{
-				$returnResults[] = $row;
+				$maillist = "";
 				
-				if ($totalCount>0)
+				while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) 
 					{
-					$maillist .= ", ";
+					$returnResults[] = $row;
+					
+					if ($totalCount>0)
+						{
+						$maillist .= ", ";
+						}
+						
+					$maillist .= $row['email'];
+					$totalCount = $totalCount+1;
 					}
 					
-				$maillist .= $row['email'];
-				$totalCount = $totalCount+1;
-				}
-				
-			$headers[] = 'From: softball@proctorfamily.org';
-			$headers[] = 'MIME-Version: 1.0';
-			$headers[] = 'Content-type: text/html; charset=iso-8859-1';
-			// - http://php.net/manual/en/function.mail.php	
-			// simple email test below - production one should be HTML and include the link to the page - example #4.
-			//		$maillist      = 'proc@comcast.net';
-			$subject = "Stats for $team_name";
+				$headers[] = 'From: softball@proctorfamily.org';
+				$headers[] = 'MIME-Version: 1.0';
+				$headers[] = 'Content-type: text/html; charset=iso-8859-1';
+				// - http://php.net/manual/en/function.mail.php	
+				// simple email test below - production one should be HTML and include the link to the page - example #4.
+				//		$maillist      = 'proc@comcast.net';
+				$subject = 'Stats for $team_name';
 
-			$message = "Hey Team,<br>";
-			$message .= "<br>Stats for $team_name was updated for $season_name.<br>";
-			$message .= "<a href='http://proctorfamily.org/softball/season.php?season=$season_name'> Stats Page </a> ";
-//			$message .= "<a href='http://localhost/season.php?season=$season_name'> Stats Page </a> ";
-			$message .= "<br><br>The Softball Attendance Site<br></p></body></html>";
+				$message = "Hey Team,<br><br>";
+				$message .= "<br>Stats for $team_name was updated for $season_name.<br>";
+				$message .= "<a href='http://proctorfamily.org/softball/season.php?season=$season_name'> Stats Page </a> ";
+	//			$message .= "<a href='http://localhost/season.php?season=$season_name'> Stats Page </a> ";
+				$message .= "<br><br>The Softball Attendance Site<br></p></body></html>";
 
-			if ($sendEmail==true)
-				{						
-				if (mail($maillist, $subject, $message, implode("\r\n",$headers)) == false)
+				if ($sendEmail==true)
 					{
-					/* Just in case there are other steps later or debugging required*/
-					$returnStatus = "fail:email - " . error_get_last()['message'];
-					echo $returnStatus;
-					echo print_r(error_get_last());
+					echo "<br><br><b><u>Sending Email</b></u><br><br>";							
+					if (mail($maillist, $subject, $message, implode("\r\n",$headers)) == false)
+						{
+						echo "<br>Email Failed<br><br>";						
+						/* Just in case there are other steps later or debugging required*/
+						$returnStatus = "fail:email - " . error_get_last()['message'];
+						echo $returnStatus;
+						echo print_r(error_get_last());
+						echo "<br><br><a href='stat_email_ind.php?seasonid=$season_id'>Send Individual Emails</a><br>";
+						}
+					else
+						{
+						echo "Success Email sent for " . date_format($gdatetime,"M-d-Y G:i") . "<br><br>";
+						$returnStatus = "success";
+						}
 					}
 				else
 					{
-					echo "Success Email sent <br><br>";
-					$returnStatus = "success";
+					$returnStatus = "Email Bypassed<BR>";
+					echo $message;
 					}
 				}
 			else
 				{
-				$returnStatus = "Email Bypassed<BR>";
-				echo $message;
+				$returnStatus = "fail:to get email addreses -" . mysqli_error($link) . "<BR>";
+				echo $returnStatus;
 				}
 			}
 		else
 			{
-			$returnStatus = "fail:to get email addreses -" . mysqli_error($link) . "<BR>";
-			echo $returnStatus;
+			echo "<b><u>FAIL - STAT TEAM not associated with Attendace team(email addresses) </b></u><br>";
 			}
 		}	
 	else
@@ -98,6 +108,24 @@ else
 	{
 	$returnStatus = "fail:get stat team-" . mysqli_error($link) . "<BR>";
 	}
+	
+echo "<br><a href='maintainance.php'>Return to Maintainance Page</a><br>";
+
+
+echo "<br>";
+$fullfilename = $_SERVER['REQUEST_URI']; 
+$fileparts = explode("?",basename($fullfilename));
+$filename = $fileparts[0];
+//echo $filename;
+//echo filemtime($filename);
+
+date_default_timezone_set('US/Eastern');
+// checking last time the contents of
+// a file were changed and formatting
+// the output of the date 
+//echo "Version:".date("y.md.Hi", filemtime($filename));
+echo "Updated: ".date("F d Y H:i:s", filemtime($filename));
+
 	
 //echo $returnStatus;
 //echo json_encode($returnResults);
